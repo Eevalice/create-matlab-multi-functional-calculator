0cf5631ee1dfdd754422f2df52f0211f55794d65
Carl Vincent Reyno <79138019+Eevalice@users.noreply.github.com>
2023-05-06 00:30:01 +0800

    Update create-laplace-transform.m

--- create-laplace-transform.m
+++ create-laplace-transform.m
@@ -4,7 +4,8 @@ syms t s % Defines the Laplace variable s and the time variable t as symbolic va
 while true 
     % Loops continuously until the user chooses to exit the program
     % Displays a menu of options for the user
-    disp('Select an option:')
+    disp('Welcome to Laplace Transform Calculator')
+    disp('Select an option or operation to perform the ff. task:')
     disp('1. Laplace Transform')
     disp('2. Inverse Laplace Transform')
     disp('3. Solve Initial Value Problem')
@@ -22,22 +23,22 @@ while true
         disp(['The Laplace transform of f(t) = ', char(f), ' is F(s) = ', char(F)])
         
     elseif menu == 2 % If the user chooses Inverse Laplace Transform
-        % Prompts the user to enter the function to take the inverse Laplace transform of
+        % signals the user to enter the function to take the inverse Laplace transform of
         F = input('Enter the function F(s): ');
 
         % Takes the inverse Laplace transform of the function and displays the result
-        f = ilaplace(F);
+        f = ilaplace(F); % refer to the documentation of inverse laplace
         disp(['The inverse Laplace transform of F(s) = ', char(F), ' is f(t) = ', char(f)])
         
     elseif menu == 3 % If the user chooses Solve Initial Value Problem
-        % Prompts the user to enter the differential equation and initial conditions
-        eqn = input('Enter the differential equation: ');
-        cond1 = input('Enter the first initial condition: ');
-        cond2 = input('Enter the second initial condition: ');
+        % signals the user to enter the differential equation and initial conditions
+        equation = input('Enter the differential equation: ');
+        condition1 = input('Enter the first initial condition: ');
+        condition2 = input('Enter the second initial condition: ');
 
         % Solves the differential equation and initial conditions and displays the result
-        sol = dsolve(eqn, [cond1, cond2]);
-        disp(['The solution to the differential equation ', char(eqn), ' with initial conditions ', char(cond1), ' and ', char(cond2), ' is y(t) = ', char(sol)])
+        solution = dsolve(equation, [conditon1, condition2]); % merging all the formulas
+        disp(['The solution to the differential equation ', char(equation), ' with initial conditions ', char(condition1), ' and ', char(condition2), ' is y(t) = ', char(solution)])
         
     elseif menu == 4 % If the user chooses Exit Program
         disp('Exiting program.')
@@ -49,3 +50,6 @@ while true
     % Adds a blank line to separate the output from different iterations of the loop
     disp(' ')
 end
+
+% Honor Pledge for Group Projects "I accept responsibility for my role in ensuring the integrity of the work submitted by the group in which I participated."
+% Group Programming Pair: Carl Vincent A. Reyno, Chris Evan S. Tapang
